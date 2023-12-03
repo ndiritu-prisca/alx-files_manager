1f475631915dd7b160193940c35586badea5d18e
ndiritu-prisca <priscandiritu@outlook.com>
2023-12-03 19:39:16 +0300

    Eslint check

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,7 +1,6 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-
 class AppController {
   static getStatus(request, response) {
     response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
